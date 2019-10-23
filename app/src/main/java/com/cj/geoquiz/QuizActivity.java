d4eba15dd26792b5641f3e8685d0820fc41dc2f8
chenjun <437456560@qq.com>
2019-10-24 00:51:27 +0800

    完成第3章挑战

--- app/src/main/java/com/cj/geoquiz/QuizActivity.java
+++ app/src/main/java/com/cj/geoquiz/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.cj.geoquiz;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -29,6 +30,10 @@ public class QuizActivity extends AppCompatActivity {
 
     private int mCurrentIndex = 0;
 
+    private boolean[] mAnswered = new boolean[mQuestionBank.length];
+    private int mAnsweredCnt = 0;
+    private int mCorrectCnt = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,19 +46,7 @@ public class QuizActivity extends AppCompatActivity {
         mQuestionTextView = findViewById(R.id.question_text_view);
 
         mTrueButton = findViewById(R.id.true_button);
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkAnswer(true);
-            }
-        });
         mFalseButton = findViewById(R.id.false_button);
-        mFalseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkAnswer(false);
-            }
-        });
 
         mNextButton = findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
@@ -107,16 +100,51 @@ public class QuizActivity extends AppCompatActivity {
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
+
+        if (mAnswered[mCurrentIndex]) {
+            mTrueButton.setEnabled(false);
+            mFalseButton.setEnabled(false);
+        } else {
+            mTrueButton.setEnabled(true);
+            mFalseButton.setEnabled(true);
+
+            mTrueButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    checkAnswer(true);
+                }
+            });
+            mFalseButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    checkAnswer(false);
+                }
+            });
+        }
     }
 
     private void checkAnswer(boolean userPressedTrue) {
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
         int messageResId;
-        if (userPressedTrue == answerIsTrue)
+        if (userPressedTrue == answerIsTrue) {
             messageResId = R.string.correct_toast;
-        else
+            ++mCorrectCnt;
+        } else
             messageResId = R.string.incorrect_toast;
 
+        mTrueButton.setEnabled(false);
+        mFalseButton.setEnabled(false);
+        mAnswered[mCurrentIndex] = true;
+
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+
+        if (++mAnsweredCnt == mQuestionBank.length) {
+            new AlertDialog.Builder(QuizActivity.this)
+                    .setIcon(R.drawable.arrow_left)
+                    .setMessage(mCorrectCnt + " / " + mQuestionBank.length)
+                    .setPositiveButton("确定", null)
+                    .create()
+                    .show();
+        }
     }
 }
