fd4d3741bbd1c73c6a18d9bbf750c469eaddf8ed
chenjun <437456560@qq.com>
2019-10-23 23:46:06 +0800

    删除无用的包引用

--- app/src/main/java/com/cj/geoquiz/QuizActivity.java
+++ app/src/main/java/com/cj/geoquiz/QuizActivity.java
@@ -1,10 +1,8 @@
 package com.cj.geoquiz;
 
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
